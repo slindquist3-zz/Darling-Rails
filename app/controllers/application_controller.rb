22f0bbab24485311072900d0def8d8b82225f6f6
slindquist3 <slindquist3@gmail.com>
2017-02-23 19:29:02 -0500

    commented out cors files and changed applicaiton controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::API
   # before_action :authenticate
+  before_filter :set_headers
+
+  def set_headers
+  if request.headers["HTTP_ORIGIN"]
+  # better way check origin
+  # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+    headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+    headers['Access-Control-Max-Age'] = '86400'
+    headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end
+
   def logged_in?
     !!current_user
   end
